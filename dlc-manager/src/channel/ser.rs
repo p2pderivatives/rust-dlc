p2pderivatives/rust-dlc

commit 7560cad237bbd516adc05085242550eff9410f3c

Fix protocol of channel renew and sub channel establish

dlc-manager/src/channel/ser.rs
@@ -58,8 +58,9 @@ impl_dlc_writeable_enum!(
     (4, SettledConfirmed, {(settle_tx, writeable), (counter_settle_adaptor_signature, {cb_writeable, write_ecdsa_adaptor_signature, read_ecdsa_adaptor_signature}), (own_settle_adaptor_signature, {cb_writeable, write_ecdsa_adaptor_signature, read_ecdsa_adaptor_signature}), (counter_next_per_update_point, writeable), (own_next_per_update_point, writeable), (timeout, writeable), (own_payout, writeable), (counter_payout, writeable) }),
     (5, Settled, {(settle_tx, writeable), (counter_settle_adaptor_signature, {cb_writeable, write_ecdsa_adaptor_signature, read_ecdsa_adaptor_signature}), (own_settle_adaptor_signature, {cb_writeable, write_ecdsa_adaptor_signature, read_ecdsa_adaptor_signature}), (own_payout, writeable), (counter_payout, writeable)}),
     (6, RenewOffered, {(offered_contract_id, writeable), (counter_payout, writeable), (is_offer, writeable), (offer_next_per_update_point, writeable), (timeout, writeable)}),
-    (7, RenewAccepted, {(contract_id, writeable), (offer_per_update_point, writeable), (accept_per_update_point, writeable), (buffer_transaction, writeable), (buffer_script_pubkey, writeable), (accept_buffer_adaptor_signature, {cb_writeable, write_ecdsa_adaptor_signature, read_ecdsa_adaptor_signature}), (timeout, writeable), (own_payout, writeable)}),
-    (8, RenewConfirmed, {(contract_id, writeable), (offer_per_update_point, writeable), (accept_per_update_point, writeable), (buffer_transaction, writeable), (buffer_script_pubkey, writeable), (offer_buffer_adaptor_signature, {cb_writeable, write_ecdsa_adaptor_signature, read_ecdsa_adaptor_signature}), (accept_buffer_adaptor_signature, {cb_writeable, write_ecdsa_adaptor_signature, read_ecdsa_adaptor_signature}), (timeout, writeable), (own_payout, writeable), (total_collateral, writeable)}),
+    (7, RenewAccepted, {(contract_id, writeable), (offer_per_update_point, writeable), (accept_per_update_point, writeable), (buffer_transaction, writeable), (buffer_script_pubkey, writeable), (timeout, writeable), (own_payout, writeable)}),
+    (8, RenewConfirmed, {(contract_id, writeable), (offer_per_update_point, writeable), (accept_per_update_point, writeable), (buffer_transaction, writeable), (buffer_script_pubkey, writeable), (offer_buffer_adaptor_signature, {cb_writeable, write_ecdsa_adaptor_signature, read_ecdsa_adaptor_signature}), (timeout, writeable), (own_payout, writeable), (total_collateral, writeable)}),
+    (15, RenewFinalized, {(contract_id, writeable), (offer_per_update_point, writeable), (accept_per_update_point, writeable), (buffer_transaction, writeable), (buffer_script_pubkey, writeable), (offer_buffer_adaptor_signature, {cb_writeable, write_ecdsa_adaptor_signature, read_ecdsa_adaptor_signature}), (accept_buffer_adaptor_signature, {cb_writeable, write_ecdsa_adaptor_signature, read_ecdsa_adaptor_signature}), (timeout, writeable), (own_payout, writeable), (total_collateral, writeable)}),
     (9, Closing, {(buffer_transaction, writeable), (contract_id, writeable), (is_initiator, writeable)}),
     (10, ClosedPunished, { (punishment_txid, writeable) }),
     (11, CollaborativeCloseOffered, { (counter_payout, writeable), (offer_signature, writeable), (close_tx, writeable), (timeout, writeable) })

dlc-manager/src/subchannel/ser.rs
@@ -6,8 +6,8 @@ use lightning::util::ser::{Readable, Writeable, Writer};
 
 use super::{
     AcceptedSubChannel, CloseAcceptedSubChannel, CloseConfirmedSubChannel, CloseOfferedSubChannel,
-    ClosingSubChannel, LnRollBackInfo, OfferedSubChannel, SignedSubChannel, SubChannel,
-    SubChannelState,
+    ClosingSubChannel, ConfirmedSubChannel, LnRollBackInfo, OfferedSubChannel, SignedSubChannel,
+    SubChannel, SubChannelState,
 };
 
 impl_dlc_writeable!(SubChannel, {
@@ -31,17 +31,18 @@ impl_dlc_writeable_enum!(SubChannelState,
     (0, Offered),
     (1, Accepted),
     (2, Confirmed),
-    (3, Signed),
-    (4, Closing),
-    (5, CloseOffered),
-    (6, CloseAccepted),
-    (7, CloseConfirmed),
-    (8, ClosedPunished)
+    (3, Finalized),
+    (4, Signed),
+    (5, Closing),
+    (6, CloseOffered),
+    (7, CloseAccepted),
+    (8, CloseConfirmed),
+    (9, ClosedPunished)
     ;;;
-    (9, OnChainClosed),
-    (10, CounterOnChainClosed),
-    (11, OffChainClosed),
-    (12, Rejected)
+    (10, OnChainClosed),
+    (11, CounterOnChainClosed),
+    (12, OffChainClosed),
+    (13, Rejected)
 );
 
 impl_dlc_writeable!(OfferedSubChannel, { (per_split_point, writeable) });
@@ -53,10 +54,21 @@ impl_dlc_writeable!(LnRollBackInfo, { (channel_value_satoshis, writeable), (valu
 impl_dlc_writeable!(AcceptedSubChannel, {
     (offer_per_split_point, writeable),
     (accept_per_split_point, writeable),
-    (accept_split_adaptor_signature, {cb_writeable, write_ecdsa_adaptor_signature, read_ecdsa_adaptor_signature}),
     (split_tx, {cb_writeable, split_tx::write, split_tx::read}),
     (ln_glue_transaction, writeable),
     (ln_rollback, writeable)
+});
+
+impl_dlc_writeable!(ConfirmedSubChannel, {
+    (own_per_split_point, writeable),
+    (counter_per_split_point, writeable),
+    (own_split_adaptor_signature, {cb_writeable, write_ecdsa_adaptor_signature, read_ecdsa_adaptor_signature}),
+    (split_tx, {cb_writeable, split_tx::write, split_tx::read}),
+    (ln_glue_transaction, writeable),
+    (counter_glue_signature, writeable),
+    (ln_rollback, writeable),
+    (prev_commitment_secret, writeable),
+    (next_per_commitment_point, writeable)
 });
 
 impl_dlc_writeable!(SignedSubChannel, {

commit 3ca9dd3b34711245373c4203cf90bc82cf14477b

fix: Avoid serialisation breaking changes when possible
Obviously, there's not much we can do if an enum variant disappears or
is renamed, or if the data associated with it changes. But we can
usually avoid changing identifiers unnecessarily.

With this patch we ensure backwards compatibility in certain
scenarios which were broken with 7560cad and 2013ad4.

dlc-manager/src/channel/ser.rs
@@ -60,8 +60,8 @@ impl_dlc_writeable_enum!(
     (6, RenewOffered, {(offered_contract_id, writeable), (counter_payout, writeable), (is_offer, writeable), (offer_next_per_update_point, writeable), (timeout, writeable)}),
     (7, RenewAccepted, {(contract_id, writeable), (offer_per_update_point, writeable), (accept_per_update_point, writeable), (buffer_transaction, writeable), (buffer_script_pubkey, writeable), (timeout, writeable), (own_payout, writeable)}),
     (8, RenewConfirmed, {(contract_id, writeable), (offer_per_update_point, writeable), (accept_per_update_point, writeable), (buffer_transaction, writeable), (buffer_script_pubkey, writeable), (offer_buffer_adaptor_signature, {cb_writeable, write_ecdsa_adaptor_signature, read_ecdsa_adaptor_signature}), (timeout, writeable), (own_payout, writeable), (total_collateral, writeable)}),
-    (9, RenewFinalized, {(contract_id, writeable), (offer_per_update_point, writeable), (accept_per_update_point, writeable), (buffer_transaction, writeable), (buffer_script_pubkey, writeable), (offer_buffer_adaptor_signature, {cb_writeable, write_ecdsa_adaptor_signature, read_ecdsa_adaptor_signature}), (accept_buffer_adaptor_signature, {cb_writeable, write_ecdsa_adaptor_signature, read_ecdsa_adaptor_signature}), (timeout, writeable), (own_payout, writeable), (total_collateral, writeable)}),
-    (10, Closing, {(buffer_transaction, writeable), (contract_id, writeable), (is_initiator, writeable)}),
+    (10, RenewFinalized, {(contract_id, writeable), (offer_per_update_point, writeable), (accept_per_update_point, writeable), (buffer_transaction, writeable), (buffer_script_pubkey, writeable), (offer_buffer_adaptor_signature, {cb_writeable, write_ecdsa_adaptor_signature, read_ecdsa_adaptor_signature}), (accept_buffer_adaptor_signature, {cb_writeable, write_ecdsa_adaptor_signature, read_ecdsa_adaptor_signature}), (timeout, writeable), (own_payout, writeable), (total_collateral, writeable)}),
+    (9, Closing, {(buffer_transaction, writeable), (contract_id, writeable), (is_initiator, writeable)}),
     (11, CollaborativeCloseOffered, { (counter_payout, writeable), (offer_signature, writeable), (close_tx, writeable), (timeout, writeable) })
     ;;
 );

dlc-manager/src/subchannel/ser.rs
@@ -31,18 +31,18 @@ impl_dlc_writeable_enum!(SubChannelState,
     (0, Offered),
     (1, Accepted),
     (2, Confirmed),
-    (3, Finalized),
-    (4, Signed),
-    (5, Closing),
-    (6, CloseOffered),
-    (7, CloseAccepted),
-    (8, CloseConfirmed),
-    (9, ClosedPunished)
+    (13, Finalized),
+    (3, Signed),
+    (4, Closing),
+    (5, CloseOffered),
+    (6, CloseAccepted),
+    (7, CloseConfirmed),
+    (8, ClosedPunished)
     ;;;
-    (10, OnChainClosed),
-    (11, CounterOnChainClosed),
-    (12, OffChainClosed),
-    (13, Rejected)
+    (9, OnChainClosed),
+    (10, CounterOnChainClosed),
+    (11, OffChainClosed),
+    (12, Rejected)
 );
 
 impl_dlc_writeable!(OfferedSubChannel, { (per_split_point, writeable) });